d526f89e8985b6957e692fbb90a0fbb1bb2da87c
Luis Dinis <luis.dinis@gmail.com>
2020-10-02 15:02:36 +0100

    V06 vailer

--- app.py
+++ app.py
@@ -9,6 +9,7 @@
 # 2020-09-30  V03 Comments and retrofit Issues Closed: #2
 # 2020-10-01  V04 Just the identifier
 # 2020-10-02  V05 Add vars display
+# 2020-10-02  V06 Add vailer-poc
 # ***********************************************
 from flask import Flask
 from flask import request
@@ -17,11 +18,12 @@ import socket
 import json
 import time
 import os
+import sys
 
 app = Flask(__name__)
 from requests.auth import HTTPBasicAuth
 
-identif = 'Hello World - Moon-V05'
+identif = 'Hello World - Moon-V06'
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -49,7 +51,7 @@ def variaveis():
         backend = os.environ['backend']
     except KeyError:
        backend = "please set env backend" 
-        #sys.exit(1)
+        sys.exit(1)
 
     return 'backend :' + backend 
     
@@ -58,6 +60,18 @@ def variaveis():
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
+
+@app.route('/vailer')
+def vailer():
+    try:
+        backend = os.environ['backend']
+    except KeyError:
+        print("please set env backend" )
+        sys.exit(1)
+    
+    url = 'http://' + backend 
+    myRespons3 = requests.get(url)
+    return myRespons3.text
 
 
 
