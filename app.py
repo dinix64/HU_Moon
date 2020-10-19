ba94a011c4f03653e47653e19667355869c889c9
Luis Dinis <luis.dinis@gmail.com>
2020-10-19 14:16:14 +0100

    Preparaçao

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ import sys
 app = Flask(__name__)
 from requests.auth import HTTPBasicAuth
 
-identif = 'Hello World - Moon-V06-05'
+identif = 'Hello World - Moon-VTeste-13'
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
